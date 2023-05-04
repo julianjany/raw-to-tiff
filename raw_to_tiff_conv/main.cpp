b737c9cb5341a56b540d690dd95398b654cc61af
julianjany <julianjany.1999@gmail.com>
2023-05-04 22:29:56 +0200

    adds improved version

--- raw_to_tiff_conv/main.cpp
+++ raw_to_tiff_conv/main.cpp
@@ -1,68 +1,93 @@
 #include <libraw.h>
 #include <tinytiffwriter.h>
 
+#include <array>
 #include <filesystem>
 #include <iostream>
+#include <vector>
+
+struct accumulator_t {
+  uint64_t sum{0};
+  uint64_t count{0};
+};
+
+int get_rgb_id(LibRaw& ip, int const row, int const col) {
+  switch (ip.imgdata.idata.cdesc[ip.COLOR(row, col)]) {
+    case 'R':
+      return 0;
+    case 'G':
+      return 1;
+    case 'B':
+      return 2;
+
+    default:
+      return -1;
+  }
+}
 
 int main() {
   LibRaw ip{};
   auto const raw_directory_path{
-      std::filesystem::path{"..\\..\\imagingresource"}};
+      std::filesystem::path{"..\\..\\photographyblog"}};
   auto const raw_files{std::filesystem::directory_iterator{raw_directory_path}};
+  size_t const scale_factor{2U};
+  static constexpr size_t channel_count{3U};
 
   for (auto const& raw_filename : raw_files) {
     ip.open_file(raw_filename.path().c_str());
     ip.unpack();
-    auto const& sizes = ip.imgdata.sizes;
-    size_t const tiff_width{sizes.width / 2U};
-    size_t const tiff_height{sizes.height / 2U};
-    size_t const tiff_pixelcount{tiff_width * tiff_height};
-    auto tiff_data{std::vector<uint16_t>(tiff_pixelcount * 3U, 0)};
-    auto tiff_green_first{std::vector<bool>(tiff_pixelcount, true)};
 
-    for (size_t row{sizes.top_margin};
-         row < (size_t)sizes.height + sizes.top_margin; ++row) {
-      for (size_t col{sizes.left_margin};
-           col < (size_t)sizes.width + sizes.left_margin; ++col) {
-        size_t const tiff_row{(row - sizes.top_margin) / 2};
-        size_t const tiff_col{(col - sizes.left_margin) / 2};
-        size_t const tiff_pixel_id{(tiff_row * tiff_width + tiff_col) * 3U};
-        size_t const raw_pixel_id{row * sizes.raw_width + col};
-        uint16_t const raw_value{ip.imgdata.rawdata.raw_image[raw_pixel_id]};
+    {
+      auto const& sizes{ip.imgdata.sizes};
+      auto const& imgdata{ip.imgdata};
+      auto const& rawdata{ip.imgdata.rawdata};
+      size_t const tiff_width{sizes.width / scale_factor};
+      size_t const tiff_height{sizes.height / scale_factor};
+      size_t const tiff_pixelcount{tiff_width * tiff_height};
+      auto tiff_data{std::vector<uint16_t>(tiff_pixelcount * channel_count, 0)};
 
-        switch (ip.imgdata.idata.cdesc[ip.COLOR(row, col)]) {
-          case 'R':
-            tiff_data[tiff_pixel_id] = raw_value;
-            break;
-          case 'G': {
-            if (tiff_green_first[tiff_pixel_id / 3]) {
-              tiff_data[tiff_pixel_id + 1] = raw_value;
-              tiff_green_first[tiff_pixel_id / 3] = false;
-            } else {
-              uint16_t const prev{tiff_data[tiff_pixel_id + 1]};
-              uint32_t const sum{static_cast<uint32_t>(prev) + raw_value};
-              tiff_data[tiff_pixel_id + 1] = static_cast<uint16_t>(sum / 2);
+      for (size_t tiff_row{0}; tiff_row < tiff_height; ++tiff_row) {
+        for (size_t tiff_col{0}; tiff_col < tiff_width; ++tiff_col) {
+          // accumulate raw values for 'scale-region'
+          auto accumulator{std::array<accumulator_t, 3>{}};
+          for (size_t raw_row_offset{0}; raw_row_offset < scale_factor;
+               ++raw_row_offset) {
+            for (size_t raw_col_offset{0}; raw_col_offset < scale_factor;
+                 ++raw_col_offset) {
+              size_t const raw_row{tiff_row * scale_factor + raw_row_offset +
+                                   sizes.top_margin};
+              size_t const raw_col{tiff_col * scale_factor + raw_col_offset +
+                                   sizes.left_margin};
+              size_t const raw_pixel_id{raw_row * sizes.raw_width + raw_col};
+              uint16_t const raw_value{rawdata.raw_image[raw_pixel_id]};
+
+              int const rgb_id{get_rgb_id(ip, raw_row, raw_col)};
+              accumulator[rgb_id].sum += raw_value;
+              accumulator[rgb_id].count++;
             }
-          } break;
-          case 'B':
-            tiff_data[tiff_pixel_id + 2] = raw_value;
-            break;
-          default:
-            break;
+          }
+          size_t const tiff_pixel_id{(tiff_row * tiff_width + tiff_col) *
+                                     channel_count};
+          for (size_t channel_id{0}; channel_id < channel_count; ++channel_id) {
+            if (accumulator[channel_id].count != 0) {
+              tiff_data[tiff_pixel_id + channel_id] =
+                  accumulator[channel_id].sum / accumulator[channel_id].count;
+            }
+          }
         }
       }
+
+      TinyTIFFWriterFile* tif = TinyTIFFWriter_open(
+          "myfile.tif", 16, TinyTIFFWriter_UInt, channel_count, tiff_width,
+          tiff_height, TinyTIFFWriter_RGB);
+      TinyTIFFWriter_writeImage(tif, tiff_data.data());
+      TinyTIFFWriter_close(tif);
+
+      std::cout << raw_filename << ": " << ip.imgdata.color.raw_bps << "bps\n";
+      std::cout << raw_filename << ": " << ip.imgdata.idata.cdesc << "colors\n";
+
+      return 0;
     }
-
-    TinyTIFFWriterFile* tif =
-        TinyTIFFWriter_open("myfil.tif", 16, TinyTIFFWriter_UInt, 3, tiff_width,
-                            tiff_height, TinyTIFFWriter_RGB);
-    TinyTIFFWriter_writeImage(tif, tiff_data.data());
-    TinyTIFFWriter_close(tif);
-
-    std::cout << raw_filename << ": " << ip.imgdata.color.raw_bps << "bps\n";
-    std::cout << raw_filename << ": " << ip.imgdata.idata.cdesc << "colors\n";
-
-    return 0;
 
     ip.recycle();
   }
